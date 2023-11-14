3b768a0ef14bd6af259fafe9942f5c70a0953b2d
Nandan Shah <nandanshah318@gmail.com>
2023-11-14 15:47:14 +0530

    third commit

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,9 @@
 const express= require("express");
-const { fileUploadController } = require("../controllers/fileUploadController");
+const { fileUploadController, getImage } = require("../controllers/fileUploadController");
 
 const router = express.Router();
 //data send krna 
 router.post("/fileUpload", fileUploadController);
+router.get('/file/:fileId', getImage);
 //export function module
 module.exports = router;
